bd2dce57c3f13808d008eafb2683cc91072bb2ef
joe-bor <kimborlongan_08@yahoo.com>
2023-09-21 16:55:12 -0700

    change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
+  // output: "server",
+  // adapter: netlify()
 });
