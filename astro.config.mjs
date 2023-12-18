cc193f9bf852fd77d0c28b2e025a692a6668e187
joe-bor <kimborlongan_08@yahoo.com>
2023-12-18 06:38:02 -0800

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import mdx from '@astrojs/mdx';
+import partytown from "@astrojs/partytown";
 
+// import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: netlify()
+  site: "https://portfolio.joe-bor.me/",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
